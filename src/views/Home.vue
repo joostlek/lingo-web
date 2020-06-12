c31207a3c506040c96d4adfaf2e876189d8ebc76
joostlek <joostlek@outlook.com>
2020-06-12 21:13:37 +0200

    Remove default code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,14 +5,10 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import Logo from '@/components/Logo'
 
 export default {
   name: 'Home',
   components: {
-    Logo,
   }
 }
 </script>
